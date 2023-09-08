18e6ee6ea0accc7c4e138d4d481aa7e3a1ce7ccb
israamawaheb <israa.mawaheb2022@gmail.com>
2023-09-09 00:17:58 +0300

    + home screen + home tabs + quran tab details

---
+++ app/src/main/java/com/example/islami/home/HomeActivity.kt
@@ -0,0 +1,53 @@
+package com.example.islami.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.example.islami.R
+import com.example.islami.databinding.ActivityHomeBinding
+import com.example.islami.home.tabs.hadeth.HadethFragment
+import com.example.islami.home.tabs.quran.QuranFragment
+import com.example.islami.home.tabs.radio.RadioFragment
+import com.example.islami.home.tabs.tasbeh.TasbehFragment
+import com.google.android.material.navigation.NavigationBarView
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var viewBinding:ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+
+        viewBinding.content.bottomNav.setOnItemSelectedListener(object :NavigationBarView.OnItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+                when(item.itemId){
+                    R.id.navigation_quran ->{
+                          showTabFragment(QuranFragment())
+                    }
+                    R.id.navigation_hadeth->{
+                          showTabFragment(HadethFragment())
+                    }
+                    R.id.navigation_radio-> {
+                           showTabFragment(RadioFragment())
+                    }
+                    R.id.navigation_tasbeh-> {
+                           showTabFragment(TasbehFragment())
+                    }
+                }
+                return true
+            }
+
+        }
+        )
+
+    }
+private fun showTabFragment(fragment:Fragment){
+          supportFragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).commit()
+   }
+}
